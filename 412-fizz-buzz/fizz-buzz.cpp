7a8182bacb30a30bf9ce1214a6e6e2875540bf94
Lukas Kavaliauskis <111790658+LukasKava@users.noreply.github.com>
2023-12-19 17:39:18 +0100

    Time: 5 ms (29.02%) | Memory: 8.2 MB (41.79%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         std::vector<string> answer;
+        std::string         tmp_string;
         for (int i = 1; i <= n; i++)
         {
-            if (i % 3 == 0 && i % 5 == 0)
-                answer.push_back("FizzBuzz");
-            else if (i % 3 == 0)
-                answer.push_back("Fizz");
-            else if (i % 5 == 0)
-                answer.push_back("Buzz");
-            else
-                answer.push_back(to_string(i));
+            tmp_string = "";
+            if (i % 3 == 0)
+                tmp_string += "Fizz";
+            if (i % 5 == 0)
+                tmp_string += "Buzz";
+            if (tmp_string.empty())
+                tmp_string = to_string(i);
+            answer.push_back(tmp_string);
         }
         return (answer);
     }
