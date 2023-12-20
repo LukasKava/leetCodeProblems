3f6f4f9503de1373df05a302e923f77af34aaf08
Lukas Kavaliauskis <111790658+LukasKava@users.noreply.github.com>
2023-12-20 19:17:25 +0100

    Time: 24 ms (5.34%) | Memory: 47 MB (29.77%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,37 +1,5 @@
 class Solution {
 public:
-    // int buyChoco(vector<int>& prices, int money) {
-    //     int first_chocolate[2] = {0, prices[0]};
-    //     int second_chocolate[2] = {0, prices[0]};
-
-    //     for (int i = 0; i < prices.size(); i++)
-    //     {
-    //         if (prices[i] < first_chocolate[1])
-    //         {
-    //             first_chocolate[0] = i;
-    //             first_chocolate[1] = prices[i];
-    //         }
-    //     }
-    //     if (first_chocolate[0] == second_chocolate[0])
-    //     {
-    //         second_chocolate[0]++;
-    //         second_chocolate[1] = prices[1];
-    //     }
-
-    //     for (int i = 0; i < prices.size(); i++)
-    //     {
-    //         if (i != first_chocolate[0])
-    //         {
-    //             if (prices[i] < second_chocolate[1])
-    //             {
-    //                 second_chocolate[0] = i;
-    //                 second_chocolate[1] = prices[i];
-    //             }
-    //         }
-    //     }
-    //     return ((first_chocolate[1] + second_chocolate[1]) > money ? money : (money - (first_chocolate[1] + second_chocolate[1])));
-    // }
-
     //smarter solution
     int buyChoco(vector<int>& prices, int money) {
         sort(prices.begin(), prices.end());
