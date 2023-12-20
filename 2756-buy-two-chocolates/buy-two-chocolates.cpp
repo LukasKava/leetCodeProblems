d292886297b012016263ad641e8118ffcbb4d4d8
Lukas Kavaliauskis <111790658+LukasKava@users.noreply.github.com>
2023-12-20 19:15:46 +0100

    Time: 4 ms (93.98%) | Memory: 47.1 MB (29.77%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -35,6 +35,8 @@ public:
     //smarter solution
     int buyChoco(vector<int>& prices, int money) {
         sort(prices.begin(), prices.end());
-        return ((prices[0] + prices[1]) > money ? money : (money - (prices[0] + prices[1])));
+        if (prices[0] + prices[1] > money)
+            return (money);
+        return (money - (prices[0] + prices[1]));
     }
 };
