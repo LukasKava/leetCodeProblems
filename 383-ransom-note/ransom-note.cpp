fc2a826a35cd7f7f86f5b88d678d6ae23cfd6fdf
Lukas Kavaliauskis <111790658+LukasKava@users.noreply.github.com>
2023-12-20 17:20:23 +0100

    Time: 11 ms (83.94%) | Memory: 9 MB (85.58%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -3,19 +3,17 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         int i;
         int lowercases[26] = {0};
-        for (i = 0; ransomNote[i] != '\0'; i++)
-        {
-            lowercases[ransomNote[i] - 'a']++;
-        }
         for (i = 0; magazine[i] != '\0'; i++)
         {
-            lowercases[magazine[i] - 'a']--;
+            lowercases[magazine[i] - 'a']++;
+        }
+        for (i = 0; ransomNote[i] != '\0'; i++)
+        {
+            lowercases[ransomNote[i] - 'a']--;
+            if (lowercases[ransomNote[i] - 'a'] < 0)
+                return (false);
         }
 
-        for (i = 0; i < 26 && lowercases[i] <= 0; i++)
-        {
-            continue;
-        }
-        return (i == 26 ? true : false);
+        return (true);
     }
 };
