f14dcb6b97b140046f2b0c26f0fa15ddb79cd21c
Lukas Kavaliauskis <111790658+LukasKava@users.noreply.github.com>
2024-01-04 20:00:35 +0100

    Time: 1 ms (94.59%) | Memory: 45 MB (6.25%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,15 +1,47 @@
 class Solution {
+    // public void duplicateZeros(int[] arr) {
+    //     int tmp = 0;
+    //     for (int i = 0; i < arr.length; i++)
+    //     {
+    //         if (arr[i] == 0)
+    //         {
+    //             for (int x = arr.length - 2; x >= i; x--)
+    //             {
+    //                 arr[x + 1] = arr[x];
+    //             }
+    //             i++;
+    //         }
+    //     }
+    // }
+    //BETTER SOLUTION
     public void duplicateZeros(int[] arr) {
-        int tmp = 0;
-        for (int i = 0; i < arr.length; i++)
+        int possible_dups = 0;
+        int length_ = arr.length - 1;
+        
+        //Find all the possible duplicating 0
+        for (int i = 0; i <= length_ - possible_dups; i++)
         {
             if (arr[i] == 0)
             {
-                for (int x = arr.length - 2; x >= i; x--)
+                if (i == length_ - possible_dups)
                 {
-                    arr[x + 1] = arr[x];
+                    arr[length_] = 0;
+                    length_--;
+                    break;
                 }
-                i++;
+                possible_dups++;
+            }
+        }
+        
+        int last_pos = length_ - possible_dups;
+        // Copy zero twice, and non zero once.
+        for (int i = last_pos; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i + possible_dups] = 0;
+                possible_dups--;
+                arr[i + possible_dups] = 0;
+            } else {
+                arr[i + possible_dups] = arr[i];
             }
         }
     }
