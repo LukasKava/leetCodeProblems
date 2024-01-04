c665f99e8507c34e4eac98a25ef359c4bb6b3da7
Lukas Kavaliauskis <111790658+LukasKava@users.noreply.github.com>
2024-01-04 19:39:28 +0100

    Time: 14 ms (32.01%) | Memory: 44.8 MB (9.07%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int tmp = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                for (int x = arr.length - 2; x >= i; x--)
+                {
+                    arr[x + 1] = arr[x];
+                }
+                i++;
+            }
+        }
+    }
+}
